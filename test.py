a27bc8626be2c18fbd8281693716810b1dfcabd6
Mehrdadep <mehrdad.smaeilpour@yahoo.com>
2018-03-20 03:13:46 +0330

    small changes in test.py

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 from InstaPic import InstaPic
 
-var = InstaPic()
-print(var.getLargePhoto('ad'))
+insta = InstaPic()
+
+#link to a 150x150 profile picture
+print(insta.getSmallPhoto('mehrdadep'))
+
+#link to a 1080x1080 profile picture
+print(insta.getLargePhoto('mehrdadep'))
